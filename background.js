aad98908dec7914527ba82db04197a10a024f5c6
Sean <seanpinchevsky@gmail.com>
2023-01-27 10:21:33 +0200

    delete the last viersion

--- background.js
+++ background.js
@@ -4,20 +4,5 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       tabId,
       tab.title.startsWith("Likes You") ? "show" : "hide"
     );
-    if (tab.title.startsWith("Discover")) {
-      var manifestData = chrome.runtime.getManifest();
-
-      fetch(
-        "https://raw.githubusercontent.com/Seadox/OKC-Show-Likes/master/manifest.json"
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          if (data.version > manifestData.version) {
-            chrome.tabs.sendMessage(tabId, "update");
-          }
-        });
-    }
   }
 });
